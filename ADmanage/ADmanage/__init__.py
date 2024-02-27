5404da708606a9b59e4481eecdd400e1a60c47cc
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2024-02-27 22:24:58 +0100

    Update __init__.py

--- ADmanage/ADmanage/__init__.py
+++ ADmanage/ADmanage/__init__.py
@@ -1,5 +1,9 @@
 import ldap3
-from ldap3 import Server, Connection, ALL, MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE
+from ldap3 import Server, Connection, ALL, SUBTREE, MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE
+from impacket.structure import Structure
+import socket
+import dns.resolver
+import datetime
 
 class ADclient:
     def __init__(self, domain, username, password, dc_ip, base_dn=None, secure=False):
@@ -10,12 +14,14 @@ class ADclient:
         self.dc_ip = dc_ip
         self.base_dn = base_dn
         self.secure = secure
+        self.domainroot = f"DC={domain.split('.')[0]},DC={domain.split('.')[1]}"
+        self.dnsroot = f"DC={domain},CN=MicrosoftDNS,DC=DomainDnsZones,{self.domainroot}"
         self.conn = self.connect_to_ldap()
 
     def connect_to_ldap(self):
         dc_url = f"ldaps://{self.dc_ip}:636" if self.secure else f"ldap://{self.dc_ip}:389"
         if not self.base_dn:
-            self.base_dn = f"DC={self.domain.split('.')[0]},DC={self.domain.split('.')[1]}"
+            self.base_dn = self.domainroot
 
         server = Server(dc_url, get_info=ALL)
         conn = Connection(server, user=self.sam, password=self.password, auto_bind=True)
@@ -23,6 +29,130 @@ class ADclient:
 
     def disconnect(self):
         self.conn.unbind()
+
+    class DNS_RECORD(Structure):
+        """
+        dnsRecord - used in LDAP
+        [MS-DNSP] section 2.3.2.2
+        """
+        structure = (
+            ('DataLength', '<H-Data'),
+            ('Type', '<H'),
+            ('Version', 'B=5'),
+            ('Rank', 'B'),
+            ('Flags', '<H=0'),
+            ('Serial', '<L'),
+            ('TtlSeconds', '>L'),
+            ('Reserved', '<L=0'),
+            ('TimeStamp', '<L=0'),
+            ('Data', ':')
+        )
+
+    class DNS_RPC_RECORD_A(Structure):
+        """
+        DNS_RPC_RECORD_A
+        [MS-DNSP] section 2.2.2.2.4.1
+        """
+        structure = (
+            ('address', ':'),
+        )
+
+        def formatCanonical(self):
+            return socket.inet_ntoa(self['address'])
+
+        def fromCanonical(self, canonical):
+            self['address'] = socket.inet_aton(canonical)
+
+    class DNS_RPC_RECORD_TS(Structure):
+        """
+        DNS_RPC_RECORD_TS
+        [MS-DNSP] section 2.2.2.2.4.23
+        """
+        structure = (
+            ('entombedTime', '<Q'),
+        )
+        def toDatetime(self):
+            microseconds = self['entombedTime'] / 10.
+            return datetime.datetime(1601,1,1) + datetime.timedelta(microseconds=microseconds)
+
+    def new_record(self, rtype):
+        nr = self.DNS_RECORD()
+        nr['Type'] = rtype
+        nr['Serial'] = self.get_next_serial(self.dc_ip, self.domain)
+        nr['TtlSeconds'] = 180
+        # From authoritive zone
+        nr['Rank'] = 240
+        return nr
+
+    def get_next_serial(self, dc, zone):
+        # Create a resolver object
+        dnsresolver = dns.resolver.Resolver()
+        dnsresolver.nameservers = [dc]
+
+        res = dnsresolver.resolve(zone, 'SOA',tcp=False)
+        for answer in res:
+            return answer.serial + 1
+
+    def get_DNSentries(self):
+        filter = "(objectClass=*)"
+        self.conn.search(search_base=self.dnsroot, search_filter=filter, search_scope=SUBTREE)
+        entries = self.conn.entries
+        return [entry.entry_dn for entry in entries if "._tcp" not in entry.entry_dn and "._udp" not in entry.entry_dn]
+
+    def get_raw_entry(self, target):
+        filter = f'(&(objectClass=dnsNode)(name={target}))'
+        self.conn.search(search_base=self.dnsroot, search_filter=filter, attributes=['dnsRecord','dNSTombstoned','name'])
+        for entry in self.conn.response:
+            if entry['type'] != 'searchResEntry':
+                continue
+            return entry
+
+    def get_DNSentry(self, target):
+        record_data = self.get_raw_entry(target)['raw_attributes']['dnsRecord'][0][-4:]
+        parsed_record = self.DNS_RPC_RECORD_A(record_data)
+        ip_address = parsed_record.formatCanonical()
+        return {'name': self.get_raw_entry(target)['attributes']['name'], 'ip': ip_address}
+
+    def add_DNSentry(self, target, data):
+        record_dn = f'DC={target},{self.dnsroot}'
+        node_data = {
+            # Schema is in the root domain (take if from schemaNamingContext to be sure)
+            'objectCategory': f'CN=Dns-Node,CN=Schema,CN=Configuration,{self.domainroot}',
+            'dNSTombstoned': False,
+            'name': target
+        }
+        record = self.new_record(1)
+        record['Data'] = self.DNS_RPC_RECORD_A()
+        record['Data'].fromCanonical(data)
+        node_data['dnsRecord'] = [record.getData()]
+        self.conn.add(record_dn, ['top', 'dnsNode'], node_data)
+        return self.get_DNSentry(target)
+
+    def modify_DNSentry(self, target, data):
+        targetentry = self.get_raw_entry(target)
+        records = []
+        for record in targetentry['raw_attributes']['dnsRecord']:
+            dr = self.DNS_RECORD(record)
+            if dr['Type'] == 1:
+                targetrecord = dr
+            else:
+                records.append(record)
+        targetrecord['Serial'] = self.get_next_serial(self.dc_ip, self.domain)
+        targetrecord['Data'] = self.DNS_RPC_RECORD_A()
+        targetrecord['Data'].fromCanonical(data)
+        records.append(targetrecord.getData())
+        self.conn.modify(targetentry['dn'], {'dnsRecord': [(MODIFY_REPLACE, records)]})
+        return self.get_DNSentry(target)
+
+    def del_DNSentry(self, target):
+        targetentry = self.get_raw_entry(target)
+        diff = datetime.datetime.today() - datetime.datetime(1601,1,1)
+        tstime = int(diff.total_seconds()*10000)
+        # Add a null record
+        record = self.new_record(0)
+        record['Data'] = self.DNS_RPC_RECORD_TS()
+        record['Data']['entombedTime'] = tstime
+        self.conn.modify(targetentry['dn'], {'dnsRecord': [(MODIFY_REPLACE, [record.getData()])],'dNSTombstoned': [(MODIFY_REPLACE, True)]})
 
     # List all user, group and computer objects
     def get_ADobjects(self):
